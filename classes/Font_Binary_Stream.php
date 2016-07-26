989e8a545e5b939cddf3174550cdd08b94909a87
Jake Jung <magoja@gmail.com>
2016-07-26 08:59:27 -0700

    Bug fix

--- classes/Font_Binary_Stream.php
+++ classes/Font_Binary_Stream.php
@@ -342,7 +342,7 @@ class Font_Binary_Stream {
           
           $ret = 0;
           for($i = 0; $i < $type[1]; $i++) {
-            $value = count($data) < $i ? $data[$i] : 0;
+            $value = $i < count($data) ? $data[$i] : 0;
             $ret += $this->w($type[0], $value);
           }
           return $ret;
